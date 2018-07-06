3d573c031beca5916a34e65565881a8bf621a4ef
maggieli96 <mli@g.hmc.edu>
2018-07-06 11:50:00 -0700

    reclassify uncertain STNUs and rerun LP

--- convert.py
+++ convert.py
@@ -5,28 +5,33 @@ import json
 import math
 
 
-filename = 'dataset/rover_coordination_small_less_activities.json'
+filename = 'car_sharing.json'
 
 with open(filename, 'r') as f:
     jsonObj = json.loads(f.read())
 
+print("Finished reading file...")
 # out directory
 strong_directory = '../../../examples/strong/'
 dynamic_directory = '../../../examples/dynamic/'
 uncertain_directory = '../../../examples/uncertain/'
 
 
-strongIndex = 9
-dynamicIndex = 5
+strongIndex = 440
+dynamicIndex = 451
 uncertainIndex = 5
+count = 1
+nameDic = {}
+probDic = {}
 for stn in jsonObj["instances"]:
     name = list(stn.keys())[0]
     jsonSTN = stn[name]
     new = STN()
     verts = {}
 
-    print("Processing:", name)
+    print("Processing:", name, count)
     count += 1
+
     for e in jsonSTN:
         start = e["start_event_name"]
         end = e["end_event_name"]
@@ -60,19 +65,29 @@ for stn in jsonObj["instances"]:
         if verts[start] in new.uncontrollables and type == 'stcu':
             type = 'stc'
 
-        new.addEdge(verts[start], verts[end], lb, ub, type=type)
+        if (verts[start], verts[end]) in new.edges or (verts[end], verts[start]) in new.edges:
+            if name not in probDic:
+                probDic[name] = [(verts[start], verts[end])]
+            else:
+                probDic[name].append((verts[start], verts[end]))
+        else:
+            new.addEdge(verts[start], verts[end], lb, ub, type=type)
+
 
     if new.isStronglyControllable():
         fname = 'strong' + str(strongIndex) + '.json'
+        nameDic[name] = fname
         new.toJSON(fname, strong_directory)
         strongIndex += 1
 
     elif dc_checking(new):
         fname = 'dynamic' + str(dynamicIndex) + '.json'
+        nameDic[name] = fname
         new.toJSON(fname, dynamic_directory)
         dynamicIndex += 1
 
     else:
         fname = 'uncertain' + str(uncertainIndex) + '.json'
+        nameDic[name] = fname
         new.toJSON(fname, uncertain_directory)
         uncertainIndex += 1
