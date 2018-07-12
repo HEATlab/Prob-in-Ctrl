757b1dbc4524ef584d4840529079ec6788c12382
maggieli96 <mli@g.hmc.edu>
2018-07-12 16:28:59 -0700

    add convert to normal graph method

--- util.py
+++ util.py
@@ -77,7 +77,7 @@ def generateChain(task, free):
         for i in range(totalEvent):
             new.addVertex(i)
 
-        L = [random.randint(0, 150) for i in range(task)]
+        L = [random.randint(0, 100) for i in range(task)]
         s = sum(L)
         L = [int(x/s*free) for x in L]
         diff = free - sum(L)
@@ -100,8 +100,58 @@ def generateChain(task, free):
         low = sum([x[0] for x in bounds])
         up = sum([x[1] for x in bounds])
         makespan = random.randint(low, up)
-        new.addEdge(0,task*2, 0, makespan)
+        new.addEdge(0,task*2+1, 0, makespan)
 
         print("Checking Consistensy...")
         if new.isConsistent():
             return new
+
+
+
+##
+# \fn normal(STN)
+# \brief convert a given STNU to its normal form labeled graph using DC_STN
+#
+# \details  a normal form labeled graph is needed for Williams algorithm.
+#           The robotbrunch DC_STN class keeps track of the upper, lower case
+#           edges, so we need to use it for labeled graph.
+#
+# @param STN  an STN to convert
+#
+# @return Return the DC_STN that is the normal form labeled graph for input STN
+#         and a dictionary storing vertices added to create normal form
+def normal(STN):
+    new = DC_STN()
+    for i in list(STN.verts.keys()):
+        new.addVertex(i)
+
+    changed = {}
+    for e in list(STN.edges.values()):
+        if not e.isContingent():
+            new.addEdge(e.i, e.j, e.Cij)
+            new.addEdge(e.j, e.i, e.Cji)
+        else:
+            if e.Cji != 0:
+                new_vert = len(new.verts)
+                changed[new_vert] = (e.i, e.j, -e.Cji, e.Cij)
+                new.addVertex(new_vert)
+
+
+                new.addEdge(e.i, new_vert, -e.Cji)
+                new.addEdge(new_vert, e.i, e.Cji)
+
+                upper = e.Cij + e.Cji
+                new.addEdge(new_vert, e.j, upper)
+                new.addEdge(e.j, new_vert, 0)
+                new.addEdge(new_vert, e.j, 0, edge_type=edgeType.LOWER,
+                                                                parent=e.j)
+                new.addEdge(e.j, new_vert, -upper, edge_type=edgeType.UPPER,
+                                                                parent=e.j)
+            else:
+                new.addEdge(e.i, e.j, e.Cij)
+                new.addEdge(e.j, e.i, e.Cji)
+                new.addEdge(e.i, e.j, -e.Cji, edge_type=edgeType.LOWER,
+                                                                parent=e.j)
+                new.addEdge(e.j, e.i, -e.Cij, edge_type=edgeType.UPPER,
+                                                                parent=e.j)
+    return new, changed
