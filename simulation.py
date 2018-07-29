aa10f7d5b3f3670deac8365e5b00fa2927e95be7
Shyan Akmal - Summer CS 2018 <sakmal@hmc.edu>
2018-07-28 20:25:17 -0700

    Added dynamic dispatch strategy. This is still not working on most cases however (although the main body is implemented).

--- simulation.py
+++ simulation.py
@@ -1,5 +1,5 @@
 from util import PriorityQueue
-from stn import STN
+from stn import STN, loadSTNfromJSONfile
 from empirical import scheduleIsValid
 import random
 
@@ -65,7 +65,7 @@ def early_execution(network: STN, realization: dict) -> bool:
         
         # Update the bounds for all other timepoints
         # We only care about events being moved later in time
-        relevant_edges = self.getEdges(activated_event)
+        relevant_edges = network.getEdges(activated_event)
         for edge in relevant_edges:
             if (edge.j == activated_event) and (edge.i not in all_uncontrollables):
                 if needs_early_update(edge, activated_event, current_time, true_weight):
@@ -75,10 +75,7 @@ def early_execution(network: STN, realization: dict) -> bool:
         # Keep track of this for next iteration of loop
         old_time = current_time
     # Check if we dispatched succesfully
-    if scheduleIsValid(network, final_schedule):
-        return False
-    else:
-        return True
+    return scheduleIsValid(network, final_schedule)
 
 
 ##
@@ -86,11 +83,73 @@ def early_execution(network: STN, realization: dict) -> bool:
 # \brief Runs an STNU simulation where the agent follows the late dynamic strategy
 #
 # @param network       STNU we will run simulation on
-# @param realization   Dictionary from contingent edge values to nonnegative reals
+# @param realization   Dictionary from uncontrollables to contingent values
 # 
 # @return A bool, which is True if and only if the execution is successful
-def late_execution(network: STN, realization: dict) -> bool:
-    return True
+def late_execution(network: STN, realization: dict, verbose = False) -> bool:
+    graph = minimize_stnu(make_graph(network))
+
+    ## Bookkeeping for events
+    all_uncontrollables = set(network.uncontrollables)
+    unused_events = set(network.verts.keys())
+    not_scheduled = PriorityQueue()
+    final_schedule = {}
+    
+    # Mapping from contingent sources to uncontrollables
+    contingent_pairs = network.contingentEdges.keys()
+    inactive_uncontrollables = {src: sink for (src, sink) in contingent_pairs}
+
+    # Initialize controllables in the queue
+    for event in unused_events:
+        if (event not in all_uncontrollables) and (event != ZERO_ID):
+            not_scheduled.push(event, graph[ZERO_ID][event])
+
+    # Assume zero timepoint is removed first
+    unused_events.remove(ZERO_ID)
+    final_schedule[ZERO_ID] = 0.0
+
+    if verbose:
+        print("Queue initialized to: ")
+        print(not_scheduled.queue)
+
+    while len(unused_events) > 0:
+        # Schedule the next event
+        current_time, event = not_scheduled.pop()
+        final_schedule[event] = current_time
+        unused_events.remove(event)
+        if verbose:
+            print(event, "was scheduled at time", current_time)
+
+        # Add uncontrollables to queue
+        if event in inactive_uncontrollables:
+            uncontrolled = inactive_uncontrollables[event]
+            delay = realization[uncontrolled]
+            not_scheduled.push(uncontrolled, current_time + delay)
+
+        # See if any other events need to be scheduled earlier
+        if event in all_uncontrollables:
+            edges = network.getEdges(event)
+            for edge in edges:
+                if (edge.i == event) and (graph[event][edge.j] > 0):
+                    new_time = current_time + graph[event][edge.j]
+                    not_scheduled.addOrDecKey(edge.j, new_time) 
+
+    success = scheduleIsValid(network, final_schedule)
+    if verbose:
+        msg = "Dispatch was successful." if success else "Dispatch failed."
+        print(msg)
+    return success
+
+##
+# \fn dispatch(network, sample_size, is_early)
+def dispatch(network: STN, sample_size: int, is_early: bool = False) -> float:
+    successes = 0
+    for sample in range(sample_size):
+        if simulate_once(network, is_early):
+            successes += 1
+    success_rate = float(successes/sample_size)
+    print(f"Dispatch was succesful {100*success_rate}% of the time.")
+    return float(successes/sample_size)
 
 ##
 # \fn simulate_once(network, is_early)
@@ -102,11 +161,11 @@ def late_execution(network: STN, realization: dict) -> bool:
 #                     strategy
 # 
 # @return A bool which is True if and only if execution is successful
-def simulate_once(network: STN, is_early: bool) -> bool:
+def simulate_once(network: STN, is_early: bool = False) -> bool:
     # Generate the realization
     realization = {}
     for nodes, edge in network.contingentEdges.items():
-        realization[node[1]] = random.uniform(-edge.Cji, edge.Cij)
+        realization[nodes[1]] = random.uniform(-edge.Cji, edge.Cij)
     
     # Run the simulation
     if is_early:
@@ -171,4 +230,104 @@ def find_bounds(network: STN) -> dict:
 # @param
 # @param
 def set_dynamic_zeropoint(network: STN):
-    return 0
+    network = network.copy()
+    largish = 1000000.0
+    
+    # Add a starting edge from the zero timepoint
+    if len(network.getEdges(ZERO_ID)) == 0:
+        if ZERO_ID not in network.verts:
+            network.addVertex(ZERO_ID)
+        
+        events = list(network.verts.keys())
+        for event in events:
+            if event != ZERO_ID:
+                network.addEdge(ZERO_ID, event, 0.0, largish)
+
+    return network
+
+##
+# \fn def make_graph(network)
+# \brief 
+#
+#
+#
+def make_graph(network: STN):
+    network = set_dynamic_zeropoint(network)
+
+    # Create the graph in adjacency list format
+    events = network.verts.keys()
+    graph = {event: {} for event in events}
+    for nodes, edge in network.edges.items():
+        i, j = nodes
+        graph[i][j] = [edge.Cij]
+        graph[j][i] = [edge.Cji]
+        if edge.type != 'stc':
+            graph[i][j].append(-edge.Cji)
+            graph[j][i].append(-edge.Cij)
+
+    return graph
+
+##
+# \fn get_weight(graph, event_1, event_2)
+def get_weight(graph, event_1, event_2) -> float:
+    weights = graph[event_1]
+    if event_2 not in weights:
+        return MAX_FLOAT
+    else:
+        if type(weights[event_2]) == list:
+            return min(weights[event_2])
+        else:
+            return weights[event_2]
+
+##
+# \fn minimize_stnu(graph)
+# \brief Uses Floyd Warshall to find minimum graph
+# 
+# 
+def minimize_stnu(graph):
+    # We'll use the list here to order the events
+    events = list(graph.keys())
+    num_events = len(events)
+    
+    # DP Table -- index as dist_table[k][event_1][event_2]
+    dist_table = [{event_1: {event_2: 0 
+        for event_2 in events if event_2 != event_1} 
+        for event_1 in events} for k in range(num_events+1)]
+    # Initialize
+    for event_1 in events:
+        for event_2 in events:
+            if event_2 != event_1:
+                dist_table[0][event_1][event_2] = get_weight(graph, event_1, event_2)
+                
+
+    for k in range(1, num_events+1):
+        for event_1 in events:
+            for event_2 in events:
+                if event_2 != event_1:
+                    old_weights = dist_table[k-1]
+                    new_weights = dist_table[k]
+                    new_event = events[k-1]
+
+                    old_path = old_weights[event_1][event_2]
+                
+                    fst_half = get_weight(old_weights, event_1, new_event)
+                    snd_half = get_weight(old_weights, new_event, event_2)
+
+                    new_path = fst_half + snd_half
+
+                    new_weights[event_1][event_2] = min(old_path, new_path)
+
+    minimized = dist_table[-1]
+    # new_graph = {event_1: {event_2: [minimized[event_1][event_2]] 
+        # for event_2 in events} for event_1 in events}
+
+    return minimized
+
+### Testing
+test_1 = loadSTNfromJSONfile("stnudata/dynamic/dynamic3.json")
+print("We have network:", test_1)
+# test_graph = make_graph(test_1)
+# print("With graph: \n", test_graph)
+# test_DP = minimize_stnu(test_graph)
+# print("\nWith DP table:", test_DP)
+dispatch(test_1, 10)
