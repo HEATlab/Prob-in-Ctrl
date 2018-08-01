ebe01cc3617e106cd53e21168eb27cd7bfc943a2
Shyan Akmal - Summer CS 2018 <sakmal@hmc.edu>
2018-08-01 14:36:04 -0700

    Created new file. This will be used to compute and store results related to accuracy of dynamic dispatch.

---
+++ empirical_dynamic.py
@@ -0,0 +1,10 @@
+##
+# \file empirical_dynamic.py
+# \brief Computes and saves data for empirically testing how well we can estimate
+#        the success of dynamic dispatch rate.
+
+def save_data():
+    # For now this is hardcoded in. If a function to compute  probabilities in the
+    # case of multiple conflicts is computed, this can be replaced.
+    UNCERTAIN_12_PROB = 0.14690255486251502
+    return True
