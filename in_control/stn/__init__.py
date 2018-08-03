e8cc9bd34cd661ea3281a88aaaaf78a461dc11ed
Shyan Akmal - Summer CS 2018 <sakmal@hmc.edu>
2018-08-03 09:11:16 -0700

    Removed all the files, since we want to actually keep track of things well.

--- in_control/stn/__init__.py
+++
@@ -1,8 +0,0 @@
-# Package __init__.py file.
-
-# This is a package for all stn classes
-
-from .stn import Vertex, Edge, STN
-from .stnjsontools import (loadSTNfromJSON,
-                          loadSTNfromJSONfile,
-                          loadSTNfromJSONobj)
