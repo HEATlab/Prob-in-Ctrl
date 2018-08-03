e26d4bbd52dd6cc39e50e030e549a1685a99e536
Shyan Akmal - Summer CS 2018 <sakmal@hmc.edu>
2018-08-03 09:04:46 -0700

    Added more meat to the .gitignore. Moved in all files except the actual STNU json files.

---
+++ in_control/empirical_dynamic.py
@@ -0,0 +1,67 @@
+import probability
+import dispatch
+
+import json
+##
+# \file empirical_dynamic.py
+# \brief Computes and saves data for empirically testing how well we can estimate
+#        the success of dynamic dispatch rate.
+# \note All the data is manually entered in
+
+def save_data():
+    out_name = "result/result_dynamic.json"
+    # This dictionary has
+    # - keys:   names of files
+    # - values: (expected success rate, actual success rates)
+    dynamic_data = {}
+
+    ## Setup for the files to use
+    # File number 22 has strange dispatch error, and is thus omitted
+    old_nums = [j for j in range(1, 32) if (j not in {22, 27})]
+    # File 17, 21, 25 through 47 had dispatch errors and are omitted
+    bad_set = {17, 21}.union(set(range(25, 48)), set(range(118, 133)), 
+            {23, 52, 57, 58, 62, 70, 72, 73, 74, 80, 81, 83, 85, 
+                94, 104, 111, 116})
+    new_nums = [j for j in range(1,139) if j not in bad_set]
+    
+    # This dictionary has
+    # - keys:   names of folders
+    # - values: (base name, list of file nums) 
+    paths = {"stnudata/uncertain/": ("uncertain", old_nums), 
+            "stnudata/more_uncertain/": ("new_uncertain", new_nums)}
+
+    end = ".json"
+
+    # How many times we dispatch on each network
+    SAMPLE_SIZE = 50000
+
+    for path, info in paths.items():
+        beg, file_nos = info
+
+        # Go through the relevant files and store the probability and dispatch rate information
+        for j in file_nos:
+            file_name = f"{path}{beg}{j}{end}"
+            print(f"Testing {beg}{j}{end}.....")
+
+            # Compute probability
+            print("Getting probability...")
+            expected_success = probability.prob_of_DC_file(file_name)
+
+            # Compute dispatch rate
+            print("Getting dispatch rate...")
+            observed_success = dispatch.simulate_file(file_name, SAMPLE_SIZE, verbose=False)
+
+            dynamic_data[f"{beg}{j}{end}"] = (expected_success, observed_success)
+
+    # Store the data
+    with open(out_name, 'w') as out_file:
+        json.dump(dynamic_data, out_file)
+
+    print("Finished storing data.")
+    print("For you, I have toiled all night. Was it worth it?")
+
+def main():
+    save_data()
+
+if __name__ == "__main__":
+    main()
